3bf2b629e5f8e194ed6051c841fd65161e914e65
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-06-09 04:25:40 +0200

    setup first SwiftUI project: BullsEye

---
+++ RGBullsEye/RGBullsEye/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by Mohsin Ali Ayub on 09.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RGBullsEye/RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Mohsin Ali Ayub on 09.06.24.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
